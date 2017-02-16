4181556a01ac0041a020b8eabacbee358fc8fab4
Michael J. Ryan <tracker1@gmail.com>
2017-02-15 18:37:14 -0700

    tweaks to integration tests

--- tools/test/integration.test.js
+++ tools/test/integration.test.js
@@ -7,13 +7,9 @@
 
 import fs from 'mz/fs';
 import path from 'path';
-import shell from 'shelljs';
 import through2 from 'through2';
 import linestream from 'line-stream';
-import bluebird from 'bluebird';
 import streamLimit from '../../dist'; // eslint-disable-line
-
-const rimraf = bluebird.promisify(require('rimraf'));
 
 function handleItem(chunk, enc, cb) {
   const ts = this;
@@ -24,19 +20,14 @@ function handleItem(chunk, enc, cb) {
 }
 
 describe('tools/test/integration', () => {
-  const tmp = path.resolve(__dirname, '../../tmp');
-
+  const tmp = path.resolve(__dirname, '../../temp');
   const inPath = path.join(tmp, 'test-input.txt');
   const outPath = path.join(tmp, 'test-output.txt');
   const randomData = Array.from(new Array(500)).map(() => `${Math.random()}\n`).join('');
 
   beforeAll(async () => {
-    shell.mkdir(tmp);
+    if (!fs.existsSync(tmp)) await fs.mkdir(tmp);
     await fs.writeFile(inPath, randomData);
-  });
-
-  afterAll(async () => {
-    await rimraf(tmp);
   });
 
   it('will create expected output', async () => {
